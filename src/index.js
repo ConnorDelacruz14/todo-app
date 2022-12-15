0256fae13b3a17c6f6cb1adb108ad4209e26b2bc
Connor Delacruz <connorjd@uci.edu>
2022-12-14 20:41:48 -0800

    base styling complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
+import TodoList from "./todo_list";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <TodoList></TodoList>
+  </React.StrictMode>
+);
